60f16cd4888d351c9731f4d094af14d03bf1afe7
ookma-kyi <40346847+ookma-kyi@users.noreply.github.com>
2021-08-11 14:55:17 -0400

    Implemented Tera templates support
    Implemented Tera templates support globally and replaced static text
    in index, route with static template.

--- src/controllers/home_controller.rs
+++ src/controllers/home_controller.rs
@@ -1,5 +1,9 @@
-use actix_web::Responder;
+use tera::{Context};
+use actix_web::{error, Error, HttpResponse, web};
 
-pub async fn index() -> impl Responder {
-    format!("Hello World!")
+pub async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
+    let ctx = Context::new();
+    let body = tmpl.render("index.html", &ctx)
+        .map_err(|_| error::ErrorInternalServerError("Template error"))?;
+    Ok(HttpResponse::Ok().content_type("text/html").body(body))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use actix_web::{App, HttpServer};
+use tera::{Tera};
 use ookma_kyi::routes::home_routes::config_home_routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        let tera = Tera::new(
+            concat!(env!("CARGO_MANIFEST_DIR"), "/src/views/**/*")
+        ).unwrap();
         App::new()
+            .data(tera)
             .configure(config_home_routes)
     })
         .bind(("127.0.0.1", 8080))?
