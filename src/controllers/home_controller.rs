acae49926353dd4cf532bd367f84b5ac3bc7ad33
ookma-kyi <40346847+ookma-kyi@users.noreply.github.com>
2021-08-13 16:12:23 -0400

    Created base layout and changed all existing views
    I have created a base layout that all views will extend from. I have
    changed all existing views to use the new base layout.

--- src/controllers/home_controller.rs
+++ src/controllers/home_controller.rs
@@ -2,7 +2,8 @@ use tera::{Context};
 use actix_web::{error, Error, HttpResponse, web};
 
 pub async fn index(tmpl: web::Data<tera::Tera>) -> Result<HttpResponse, Error> {
-    let ctx = Context::new();
+    let mut ctx = Context::new();
+    ctx.insert("title", "Home Page");
     let body = tmpl.render("index.html", &ctx)
         .map_err(|_| error::ErrorInternalServerError("Template error"))?;
     Ok(HttpResponse::Ok().content_type("text/html").body(body))
