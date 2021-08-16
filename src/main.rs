ad5fa1c79b985d631c29986c5086f4a009777f4b
ookma-kyi <40346847+ookma-kyi@users.noreply.github.com>
2021-08-16 15:19:08 -0400

    Added support for serving static files
    Added support for serving static files such as Cascading Style Sheets,
    Javascript, images and other assets from the public directory.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use actix_files as fs;
 use actix_web::{App, HttpServer};
 use tera::{Tera};
 use ookma_kyi::routes::home_routes::config_home_routes;
@@ -10,6 +11,9 @@ async fn main() -> std::io::Result<()> {
         ).unwrap();
         App::new()
             .data(tera)
+            // static files
+            .service(fs::Files::new("/static", "src/public"))
+            // routes
             .configure(config_home_routes)
     })
         .bind(("127.0.0.1", 8080))?
